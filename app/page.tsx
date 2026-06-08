291e30d9da84d3d8ded83d396374d6fbd1b88cf5
ep25097 <ep25097-7485@gs4e.chubu.ac.jp>
2026-06-08 11:43:15 +0900

    change home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,18 @@
-"use client";
-
-import { db } from "@/lib/firebase";
-import { collection, addDoc } from "firebase/firestore";
-
 export default function Home() {
-
-  const saveData = async () => {
-
-    try {
-
-      await addDoc(collection(db, "test"), {
-        message: "hello firebase",
-        createdAt: new Date()
-      });
-
-      alert("保存成功");
-
-    } catch (error) {
-
-      console.error(error);
-
-      alert("エラー発生");
-    }
-  };
-
   return (
     <main style={{ padding: "40px" }}>
-      <button
-        onClick={saveData}
-        style={{
-          padding: "20px",
-          fontSize: "24px",
-          backgroundColor: "green",
-          color: "white",
-          border: "none",
-          cursor: "pointer"
-        }}
-      >
-        Firestore保存
-      </button>
+      <h1>クイズアプリ</h1>
+
+      <p>メニュー</p>
+
+      <ul>
+        <li>
+          <a href="/quiz">クイズ開始</a>
+        </li>
+        <li>
+          <a href="/ranking">ランキング</a>
+        </li>
+      </ul>
     </main>
   );
 }
